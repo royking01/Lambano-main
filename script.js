3d3f3f68bcbc248925e62398423a664a35d19502
royking01 <60018962+royking01@users.noreply.github.com>
2021-02-10 02:41:00 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,52 +2,28 @@ let data = document.getElementById('data');
 let variants = document.getElementById("variants");
 let amount = document.getElementById("amount");
 let item = document.getElementById("item");
-// let amnt = document.getElementById("amnt")[0].value;
 let amnt = document.getElementsByTagName("input")[0].value;
 let bag = [];
 let x = ''
 let j = []
 
-// let x =  `https://wa.me/15551234567?text=I%20am%20interested%20in${bag[0]}r%20sale`;
 amount.style.display = 'none'
-variants.addEventListener('change',async (e)=>{
-	d = e.target.value
- if('change' && d != ''){
-	bag.push (d);
-	let a = prompt(`how many packs of ${d} plantain are you interested in please`);
-	if(a > 0){
+variants.addEventListener('change', async (e) => {
+    d = e.target.value
+    if ('change' && d != '') {
 
-	let o = prompt('what price range are you orderding from please? our retail prices range from N200,N500,N1000 and N2500')
-
-		let j =	 a + " packs" + " " + bag[0] +" " +'plantain' +" " + o; 
-alert(`thank you for orderding ${j}, you will be redirected to sales now, thank you`)
-	
-		location.href = `https://wa.me/2348155386242?text=Good%20afternoon%20please%20%20%20I%20am%20interested%20in%20${a}%20packs%20of${o}%20${d}chips%20for%20sale`;
-	
-
-console.log(j)
-
-// let x =  `https://wa.me/2348155386242?text=I%20am%20interested%20${j}%20sale`;
-
-// let l = prompt('is that all please enter YES or NO')
-// if(l == 'yes' || l =='Yes' || l== 'YES'){
-// 	location.href = `https://wa.me/2348155386242?text=Good%20afternoon%20please%20%20%20I%20am%20interested%20in%${j}%20for%20sale`;
-// }
-// if(l == 'no' || l =='No' || l== 'NO'){
-
-
-
-// 	location.href = `https://wa.me/2348155386242?text=Good%20afternoon%20please%20%20%20I%20am%20interested%20in%${j}%20for%20sale`;
-// }
-}
-	// console.log(bag)
-	console.log(d)
-}
-
+        bag.push(d);
+        let a = prompt(`how many packs of ${d} plantain are you interested in please`);
+        if (a > 0) {
+            let o = prompt('what price range are you orderding from please? our retail prices range from N200,N500,N1000 and N2500');
+            let j = a + " packs" + " " + bag[0] + " " + 'plantain' + " " + o;
+            alert(`thank you for orderding ${j}, you will be redirected to sales now, thank you`)
+            location.href = `https://wa.me/2348155386242?text=Good%20afternoon%20please%20%20%20I%20am%20interested%20in%20${a}%20packs%20of%20w${o}%20${d}chips%20for%20sale`;
+            console.log(j)
+        }
+    }
 })
+async data.addEventListener('click', () => {
 
-data.addEventListener('click',()=>{
-	location.href = `https://wa.me/2348155386242?text=Good%20afternoon%20please%20%20%20I%20am%20interested%20in%20chips%20for%20sale`;
+    location.href = `https://wa.me/2348155386242?text=Good%20afternoon%20please%20%20%20I%20am%20interested%20in%20chips%20for%20sale`;
 });
-
-
